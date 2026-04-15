b092c4e7bd0cf6946a61a94d906b0fb5d591e106
Ahmed Bilal Khan <ahmedbilalkhangl09@gmail.com>
2026-04-15 12:56:39 +0500

    fix: change WebSocket route from ws/room/ to ws/interview/ to match frontend connection URL

--- interviews/routing.py
+++ interviews/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from interviews.consumers import SignalingConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_id>[a-zA-Z0-9]+)/$', SignalingConsumer.as_asgi()),
+    re_path(r'ws/interview/(?P<room_id>[a-zA-Z0-9]+)/$', SignalingConsumer.as_asgi()),
 ]
